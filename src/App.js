ef01dd39e8ef43fd72b83cfbe355697f64fcc0a8
Gokul <gokulchandan24@gmail.com>
2024-06-29 17:03:43 +0530

    created footer component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ import Projects from "./components/Projects/Projects";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Navbar from "./components/Navbar/Navbar";
+import { useState, createContext } from "react";
+import ReactSwitch from "react-switch";
+import Footer from "./components/Footer/Footer";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div>
-      <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <Navbar />
+        <div className="switch">
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="showBox">Home</div>;
+  return (
+    <div style={{ maxHeight: "100vh" }} className="Home_section showBox">
+      Home
+      Home
+      Home
+
+      H
+      <h1>
+        Home
+      </h1>
+      <h1>
+        Home
+      </h1>
+      <h1>
+        Home
+      </h1>
+      <h1>
+        Home
+      </h1>
+      <h1>
+        Home
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
